901937336f03afd2d5790a762eb1ed59a6a476d1
shenqianjin <shenqianjin@qiniu.com>
2021-12-12 18:43:31 +0800

    updated

--- gogogo.go
+++ gogogo.go
@@ -1,7 +1,9 @@
 package go_samples
 
-import "github.com/google/uuid"
+// consider to use git hook to auto-generaeted this file
 
-func abstract() {
+import _ "github.com/google/uuid"
+
+/*func abstract() {
 	uuid.ClockSequence()
-}
+}*/
